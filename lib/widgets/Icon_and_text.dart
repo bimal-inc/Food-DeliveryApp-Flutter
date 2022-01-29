82e26efe907f32c5ec80a6371ec4eed542998948
Bimal Yadav <bimalyadavemail7@gmail.com>
2022-01-29 18:22:46 +0545

    new reusable widget

---
+++ lib/widgets/Icon_and_text.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/widgets/smallText.dart';
+
+class IconAndText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  final Color IconColor;
+
+  const IconAndText(
+      {Key? key,
+      required this.icon,
+      required this.text,
+      required this.IconColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: IconColor,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        SmallText(
+          text: text,
+        )
+      ],
+    );
+  }
+}
